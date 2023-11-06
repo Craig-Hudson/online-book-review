61ccf21e463d421aec9346609524b03142e25da9
Craig Hudson <craighudson1211@gmail.com>
2023-11-06 00:04:36 +0000

    EDIT: modify USer table to add relationship with reviews and books tables

--- readersrealm/models.py
+++ readersrealm/models.py
@@ -9,6 +9,9 @@ class User(db.Model):
     email = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
     password_confirmation = db.Column(db.String(255), nullable=False)
+    reviews = db.relationship('Review', backref='user', lazy=True)
+    books = db.relationship('Book', backref='user', lazy=True)
+
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}')"
