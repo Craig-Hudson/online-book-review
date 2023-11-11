54d4682265278eba90c33a1ea4379bd23298e28a
Craig Hudson <craighudson1211@gmail.com>
2023-11-11 00:53:15 +0000

    ADD config settings for setting up email for use in contact us form

--- run.py
+++ run.py
@@ -2,9 +2,14 @@ import os
 from flask import Flask
 from readersrealm import app, db
 from flask_migrate import Migrate
+from flask_mail import Mail, Message
+
+
 
 # Initialize Flask-Migrate
 migrate = Migrate(app, db)
+
+
 
 if __name__ == "__main__":
     app.run(
@@ -12,6 +17,20 @@ if __name__ == "__main__":
         port=int(os.environ.get("PORT")),
         debug=os.environ.get("DEBUG") == "True"
     )
+
+# mail_settings = { 
+#     "MAIL_SERVER": os.environ.get('MAIL_SERVER'), 
+#     "MAIL_PORT": os.environ.get('MAIL_PORT'), 
+#     "MAIL_USE_TLS": False, 
+#     "MAIL_USE_SSL": os.environ.get('MAIL_USE_SSL'), 
+#     "MAIL_USERNAME": os.environ.get('MAIL_USERNAME'), 
+#     "MAIL_PASSWORD": os.environ.get('MAIL_PASSWORD'), 
+#     "SECURITY_EMAIL_SENDER": 
+# os.environ.get("SECURITY_EMAIL_SENDER") 
+# }
+
+# app.config.update(mail_settings)
+# mail = Mail(app)
 
 @app.shell_context_processor
 def make_shell_context():
