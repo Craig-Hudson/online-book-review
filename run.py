7497efeb993393d41915eab9ff83be65358c65c0
Craig Hudson <craighudson1211@gmail.com>
2023-12-13 23:40:25 +0000

    DELETE: delete commented out code.

--- run.py
+++ run.py
@@ -14,19 +14,6 @@ if __name__ == "__main__":
         debug=os.environ.get("DEBUG") == "True"
     )
 
-# mail_settings = { 
-#     "MAIL_SERVER": os.environ.get('MAIL_SERVER'), 
-#     "MAIL_PORT": os.environ.get('MAIL_PORT'), 
-#     "MAIL_USE_TLS": False, 
-#     "MAIL_USE_SSL": os.environ.get('MAIL_USE_SSL'), 
-#     "MAIL_USERNAME": os.environ.get('MAIL_USERNAME'), 
-#     "MAIL_PASSWORD": os.environ.get('MAIL_PASSWORD'), 
-#     "SECURITY_EMAIL_SENDER": 
-# os.environ.get("SECURITY_EMAIL_SENDER") 
-# }
-
-# app.config.update(mail_settings)
-# mail = Mail(app)
 
 @app.shell_context_processor
 def make_shell_context():
